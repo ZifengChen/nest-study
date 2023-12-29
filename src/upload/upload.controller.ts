39e7c4dfae9cbe60fd002d290b4c17442f042b2d
chenzifeng <chen1677@purdue.edu>
2023-12-29 13:45:16 +0800

    feat: d3-直接下载和流下载

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -8,11 +8,15 @@ import {
   Delete,
   UseInterceptors,
   UploadedFile,
+  Res,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { CreateUploadDto } from './dto/create-upload.dto';
 import { UpdateUploadDto } from './dto/update-upload.dto';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { zip } from 'compressing';
+import { join } from 'path';
 
 @Controller('upload')
 export class UploadController {
@@ -22,7 +26,22 @@ export class UploadController {
   @UseInterceptors(FileInterceptor('file'))
   upload(@UploadedFile() file) {
     console.log(file);
-
     return '12333';
+  }
+
+  @Get('export')
+  download(@Res() res: Response) {
+    const url = join(__dirname, '../images/1703755025645.jpeg');
+    res.download(url);
+  }
+
+  @Get('stream')
+  async down(@Res() res: Response) {
+    const url = join(__dirname, '../images/1703755025645.jpeg');
+    const tarStream = new zip.Stream();
+    await tarStream.addEntry(url);
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', `attachment;filename=zifeng`);
+    tarStream.pipe(res);
   }
 }
