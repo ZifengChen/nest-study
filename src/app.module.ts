82933c9b08fd1b7024303a633e5ff3f9c839a10b
chenzifeng <chen1677@purdue.edu>
2024-01-26 14:52:56 +0800

    feat: 守卫

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ConfigModule } from './config/config.module';
 import { UploadModule } from './upload/upload.module';
 import { LoginModule } from './login/login.module';
 import { SpiderModule } from './spider/spider.module';
+import { GuardModule } from './guard/guard.module';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot({ path: '/zifeng' }), UploadModule, LoginModule, SpiderModule],
+  imports: [UserModule, ConfigModule.forRoot({ path: '/zifeng' }), UploadModule, LoginModule, SpiderModule, GuardModule],
   controllers: [AppController],
   providers: [AppService],
 })
